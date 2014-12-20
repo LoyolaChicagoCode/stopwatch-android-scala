c8c25486acb0580cdea02cb744a3494ef69e2703
Konstantin Läufer <laufer@cs.luc.edu>
2014-12-20 13:59:17 -0600

    revised another test comment for use of terminology
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/test/scala/ui/AbstractViewUpdaterTest.scala
+++ src/test/scala/ui/AbstractViewUpdaterTest.scala
@@ -7,14 +7,15 @@ import org.scalatest.mock.MockitoSugar
 import common.ModelStateId
 
 /**
- * An abstract GUI-based functional test for the clickcounter app.
- * This follows the XUnit Testcase Superclass pattern.
+ * An abstract unit test for the view updater.
+ * This is an interaction test of an object with dependencies
+ * (collaborators); specifically to Scala, one of the dependencies
+ * is a stackable trait (mixin). We can address this by mixing in
+ * a test spy wrapper between the actual SUT and the injected activity.
+ * This test follows the XUnit Testcase Superclass pattern.
  */
 trait AbstractViewUpdaterTest extends MockitoSugar with ViewTestHelper {
 
-  // Scala-specific: one of the collaborators is a stackable trait (mixin).
-  // We can address this by mixing in a test spy wrapper between
-  // the actual SUT and the injected activity.
   trait TypedActivitySpy extends TypedActivityHolder {
     lazy val activitySpy = spy(activity)
     override def findView[T](id: TypedResource[T]): T = activitySpy.findView(id)
